a767d72cd5ed3e2ccdae374704c8a80c6a3f8f30
kota65535 <kota65535@gmail.com>
2016-12-19 00:13:17 +0900

    update

--- tasks/get_latest_version.sh
+++ tasks/get_latest_version.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Ensure this script runs in the become_user's home directory
+cd ~
+# Source pyenv initialization script
 source $1
 version_regex_str=$(echo "$2" | sed -e 's/x/[[:digit:]]/g' -e 's/\./\\./g')
 regex_str="^[[:space:]]*${version_regex_str}[[:digit:]]*$"
