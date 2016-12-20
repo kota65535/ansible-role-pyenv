1d41bf364e74783325c98535fa03595a7bcb1da9
kota65535 <kota65535@gmail.com>
2016-12-20 17:57:14 +0900

    support ansible 2.2.0 and allow empty pyenv_virtualenvs

--- tasks/get_latest_version.sh
+++ tasks/get_latest_version.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-# Ensure this script runs in the become_user's home directory
-cd ~
-# Source pyenv initialization script
-source $1
+tmpdir=$(mktemp -d)
+git clone -q $1 ${tmpdir}
+export PYENV_ROOT=${tmpdir}
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(${PYENV_ROOT}/bin/pyenv init -)"
+
 version_regex_str=$(echo "$2" | sed -e 's/x/[[:digit:]]/g' -e 's/\./\\./g')
 regex_str="^[[:space:]]*${version_regex_str}[[:digit:]]*$"
 pyenv install -l | grep -e "${regex_str}" | tail -n 1 | sed -e 's/[[:blank:]]//g'
