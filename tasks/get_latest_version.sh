eb97d328783d50486fd0e36f7e222262090fcdfb
kota65535 <kota65535@gmail.com>
2017-02-12 13:50:42 +0900

    update for others

--- tasks/get_latest_version.sh
+++ tasks/get_latest_version.sh
@@ -7,4 +7,4 @@ eval "$(${PYENV_ROOT}/bin/pyenv init -)"
 
 version_regex_str=$(echo "$2" | sed -e 's/x/[[:digit:]]/g' -e 's/\./\\./g')
 regex_str="^[[:space:]]*${version_regex_str}[[:digit:]]*$"
-pyenv install -l | grep -e "${regex_str}" | tail -n 1 | sed -e 's/[[:blank:]]//g'
+pyenv install -l | grep -e "${regex_str}" | tail -n 1 | perl -pe 's/\s//g'
