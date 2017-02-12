b3c74e6ce471d6f4b9614b1d5b9558aba1ed3059
kota65535 <kota65535@gmail.com>
2017-02-13 04:53:32 +0900

    use perl Sort::Versions

--- tasks/get_latest_version.sh
+++ tasks/get_latest_version.sh
@@ -9,4 +9,4 @@ eval "$(${PYENV_ROOT}/bin/pyenv init -)"
 
 version_regex_str=$(echo "$2" | perl -pe 's/X/\\d/g; s/\./\\./g')
 regex_str="^\s*${version_regex_str}$"
-pyenv install -l | perl -nle "print if /${regex_str}/" | sort -V | tail -n 1 | perl -pe 's/\s//g'
+pyenv install -l | perl -nle "print if /${regex_str}/" | perl -e 'use Sort::Versions; print sort versions <STDIN>' | tail -n 1 | perl -pe 's/\s//g'
