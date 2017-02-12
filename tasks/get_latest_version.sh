5c731d6177a42c5e7c8e8017d54ea8b56c7cb880
kota65535 <kota65535@gmail.com>
2017-02-12 23:17:33 +0900

    changed placeholder from 'x' to 'X'

--- tasks/get_latest_version.sh
+++ tasks/get_latest_version.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+
 tmpdir=$(mktemp -d /tmp/XXXX)
+# Clone repository and set path
 git clone -q $1 ${tmpdir}
 export PYENV_ROOT=${tmpdir}
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(${PYENV_ROOT}/bin/pyenv init -)"
 
-version_regex_str=$(echo "$2" | sed -e 's/x/[[:digit:]]/g' -e 's/\./\\./g')
-regex_str="^[[:space:]]*${version_regex_str}[[:digit:]]*$"
-pyenv install -l | grep -e "${regex_str}" | tail -n 1 | perl -pe 's/\s//g'
+version_regex_str=$(echo "$2" | perl -pe 's/X/\\d/g; s/\./\\./g')
+regex_str="^\s*${version_regex_str}$"
+pyenv install -l | perl -nle "print if /${regex_str}/" | sort -V | tail -n 1 | perl -pe 's/\s//g'
