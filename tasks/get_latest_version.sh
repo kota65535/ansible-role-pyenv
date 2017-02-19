84830bfb16fe0dc418842966a915d099e191c8c6
kota65535 <kota65535@gmail.com>
2017-02-19 22:01:58 +0900

    Add MacOSX support

--- tasks/get_latest_version.sh
+++ tasks/get_latest_version.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 tmpdir=$(mktemp -d /tmp/XXXX)
-# Clone repository and set path
-git clone -q $1 ${tmpdir}
-export PYENV_ROOT=${tmpdir}
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(${PYENV_ROOT}/bin/pyenv init -)"
+if ! which -s pyenv; then
+    # If pyenv is not installed, clone repository and set path
+    git clone -q $1 ${tmpdir}
+    export PYENV_ROOT=${tmpdir}
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(${PYENV_ROOT}/bin/pyenv init -)"
+fi
 
 version_regex_str=$(echo "$2" | perl -pe 's/X/\\d/g; s/\./\\./g')
 regex_str="^\s*${version_regex_str}$"
