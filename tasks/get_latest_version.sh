dbb8a7add8c85211fd5913ca46641dfc71b77081
Tomohiko Ozawa <kota65535@gmail.com>
2017-04-29 16:39:09 +0900

    Update get_latest_version.sh

--- tasks/get_latest_version.sh
+++ tasks/get_latest_version.sh
@@ -9,6 +9,6 @@ if ! which -s pyenv; then
     eval "$(${PYENV_ROOT}/bin/pyenv init -)"
 fi
 
-version_regex_str=$(echo "$2" | perl -pe 's/X/\\d/g; s/\./\\./g')
+version_regex_str=$(echo "$2" | perl -pe 's/X/\\d+/g; s/\./\\./g')
 regex_str="^\s*${version_regex_str}$"
 pyenv install -l | perl -nle "print if /${regex_str}/" | perl -e 'use Sort::Versions; print sort versions <STDIN>' | tail -n 1 | perl -pe 's/\s//g'
